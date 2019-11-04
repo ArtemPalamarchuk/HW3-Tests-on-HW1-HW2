6d415ef8d22e0fc4a8c725b8a3086bc6b6a2d3d0
Foks991 <33574193+Foks991@users.noreply.github.com>
2019-11-04 23:54:48 +0200

    Add Java Tests

---
+++ HW1 + Tests/JAVA/Conditional operators/tests/test1.java
@@ -0,0 +1,21 @@
+package Task1.tests;
+
+import Task1.task1;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class test1 {
+    public void t1() throws Exception{
+        assertEquals(6.0, task1.forTest(2,3),0);
+    }
+    public void t2()throws Exception{
+        assertEquals(5.0,task1.forTest(3,2),0);
+    }
+    public void t3()throws Exception{
+        assertEquals(14.5,task1.forTest(12.5,2),0);
+    }
+    public void t4()throws Exception{
+        assertEquals(14.6,task1.forTest(12.6,2),0);
+    }
+}
